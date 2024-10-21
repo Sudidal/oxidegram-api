996dc69ce6d9cf4f2ef79f47e6035647f3e4caf3
oxide <mohammedhaider15mo@gmail.com>
2024-10-21 16:12:21 +0300

    Test: More testing

---
+++ tests/login.test.js
@@ -0,0 +1,40 @@
+import app from "../app.js";
+import supertest from "supertest";
+import { resetDb } from "../utils/resetDb.js";
+
+beforeAll(async () => {
+  await resetDb();
+  await supertest(app).post("/register").type("form").send({
+    username: "oxide",
+    email: "mohammed@gmail.com",
+    password: "qwerty",
+    confirm_password: "qwerty",
+  });
+});
+
+describe("Login Works Fine", () => {
+  test("Should login", (done) => {
+    supertest(app)
+      .post("/login")
+      .type("form")
+      .send({
+        username: "oxide",
+        password: "qwerty",
+      })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .expect({ message: "Login successfull" }, done);
+  });
+  test("Should not login", (done) => {
+    supertest(app)
+      .post("/login")
+      .type("form")
+      .send({
+        username: "oxide",
+        password: "qwerty WHOPS",
+      })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .expect({ message: "Login successfull" }, done);
+  });
+});
--- tests/register.test.js
+++ tests/register.test.js
@@ -1,27 +1,37 @@
-import express from "express";
-import { baseRouter } from "../routers/baseRouter.js";
+import app from "../app.js";
 import supertest from "supertest";
 import { resetDb } from "../utils/resetDb.js";
-
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use("/", baseRouter);
 
 beforeAll(async () => {
   await resetDb();
 });
 
-test("Account register works", (done) => {
-  supertest(app)
-    .post("/register")
-    .type("form")
-    .send({
-      username: "oxide",
-      email: "mohammed@gmail.com",
-      password: "qwerty",
-      confirm_password: "qwerty",
-    })
-    .expect("Content-Type", /json/)
-    .expect({ message: "Account registered successfully" })
-    .expect(200, done);
+describe("Account Register Works Fine", () => {
+  test("Should register account", (done) => {
+    supertest(app)
+      .post("/register")
+      .type("form")
+      .send({
+        username: "oxide",
+        email: "mohammed@gmail.com",
+        password: "qwerty",
+        confirm_password: "qwerty",
+      })
+      .expect("Content-Type", /json/)
+      .expect({ message: "Account registered successfully" })
+      .expect(200, done);
+  });
+  test("Validation works, No duplicate username & email", (done) => {
+    supertest(app)
+      .post("/register")
+      .type("form")
+      .send({
+        username: "oxide",
+        email: "mohammed@gmail.com",
+        password: "qwerty",
+        confirm_password: "qwerty",
+      })
+      .expect("Content-Type", /json/)
+      .expect(400, done);
+  });
 });
