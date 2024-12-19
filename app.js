f581b27f259af2edf6d65f7b260efb8edd59a6b6
oxide <mohammedhaider15mo@gmail.com>
2024-12-19 14:05:50 +0300

    Feat: Allow only specific origin to cors

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ import express from "express";
 import configurePassport from "./passportConfig.js";
 import cors from "cors";
 import errorHandler from "./middleware/errorHandler.js";
+import getEnv from "./utils/getEnv.js";
 import { baseRouter } from "./routers/baseRouter.js";
 
 const app = express();
 
 configurePassport();
 
-app.use(cors());
+app.use(cors({ origin: getEnv("ALLOWED_ORIGIN") }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
